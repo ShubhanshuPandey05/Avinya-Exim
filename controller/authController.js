2e6ceec30b5f64ef98d1648187db0ea842c5aac2
Shubhanshu Pandey <webdevshubhanshu@gmail.com>
2025-01-18 15:23:58 +0530

    adding logo

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import generateAndSetCookies from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 import fetch from 'node-fetch';
